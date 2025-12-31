037df28808e3a0056ca97be13953e81c0ba9cf3a
Jason Howmans <jason@nosaj.io>
2025-12-31 21:05:41 +0000

    remove unsafe_cache in favor of "use cache" directive

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,7 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
 import matter from "gray-matter";
-import { unstable_cache } from "next/cache";
 import type { Post } from "./types";
 
 const BLOG_DIR = path.join(process.cwd(), "blog");
@@ -106,21 +105,3 @@ export function getAllPosts(): Post[] {
     .filter((post): post is Post => post !== null)
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 }
-
-/**
- * Cached version of getAllPosts for use in pages
- */
-export const getCachedPosts = unstable_cache(
-  async () => getAllPosts(),
-  ["blog-posts"],
-  { revalidate: 3600 },
-);
-
-/**
- * Cached version of getPostBySlug for use in pages
- */
-export const getCachedPostBySlug = unstable_cache(
-  async (slug: string) => getPostBySlug(slug),
-  ["blog-post"],
-  { revalidate: 3600 },
-);
